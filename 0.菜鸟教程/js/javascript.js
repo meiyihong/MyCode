07fdeeb8146138fcaa179acedc0e0455c8a47619
meiyihong <875468582@qq.com>
2016-11-30 20:48:31 +0800

    update

---
+++ 0.菜鸟教程/js/javascript.js
@@ -0,0 +1,111 @@
+var hello = 'heheda'
+
+function doo() {
+    alert(hello);
+    document.write(hello + hello);
+}
+
+var huohuo = function timehold() {
+    var nowtime = new Date().getHours();
+    if (nowtime > 24) {
+        document.write('heheda 时间超过20：00');
+    } else {
+        alert('时间小于20：00');
+        document.getElementById('btn').innerHTML('时间小于20：00');
+    }
+}
+
+function butn1() {
+    var x = document.getElementById("butn");
+    x.innerHTML = "改变元素内容";
+}
+
+
+function doo1() {
+    var person = {
+        firstName: "Jion",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue"
+    };
+    document.getElementById("obj1").innerHTML = person.firstName + "现在" + person.age + time;
+
+}
+
+function doo2(can1, can2, can3) {
+    alert(can1 + can2 + can3);
+}
+var person = {
+    firstName: "Jion",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
+// doo2(person.age, person.lastName, person.age);
+
+
+function numbers() {
+    var y = document.getElementById("txt").value;
+    if (y > 0 || y < 10 || y == 10) {
+        document.getElementById("tips").innerHTML = "输入" + y + "正确";
+    } else {
+        document.getElementById("tips").innerHTML = "输入" + y + "错误";
+    }
+}
+
+function emails() {
+
+}
+
+
+
+
+//取得数组的最大值
+var num1 = [1, 3, 5, 6, 3, 4, 5, 7, 8];
+
+function maxunm(num1) {
+
+    var yt = 0,
+        i = 0;
+    for (var i = 0; i < arguments[0].length; i++) {
+
+        if (arguments[0][i] > yt) {
+            yt = arguments[0][i];
+        }
+    }
+    var xk = document.getElementById("maxnumber");
+    xk.innerHTML = yt;
+}
+
+//函数调用方法
+// 作为函数调用
+// 作为全局对象调用
+// 作为方法调用
+// 使用构造函数调用
+//使用函数方法调用函数
+function myFunction(a, b) {
+    return a * b;
+}
+//apply方法调用函数
+myArry = [10, 2];
+myFunction.apply(myobject, myArry);
+//call方法调用函数
+myFunction, call(myobject, 10, 2);
+
+//全选
+windows.onload = function() {
+
+    var listall = document.getElementById("list1");
+    var all1 = listall.getElementsByTagName("input");
+    all1[0].onclick = function() {
+
+        for (var i = 1; i < all1.length; i++) {
+            if (all1[0].onclick = "all1[0].checked=true") {
+                all1[i].checked = false;
+            } else {
+                all1[0].checked = true;
+                all1[i].checked = true;
+            }
+        }
+    }
+}
